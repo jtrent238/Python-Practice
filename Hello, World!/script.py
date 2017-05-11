aef101248f3376a765268b6ab6e738735c160d8e
Trent Patterson <jtrent238@outlook.com>
2017-05-11 09:37:21 -0400

    Add files via upload

---
+++ Hello, World!/script.py
@@ -0,0 +1,11 @@
+print("This line will be printed.")
+print("TEST This is something to be printed")
+print("Something Else to Print")
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+	
+	print("Hello, World!")
+	print("Goodbye, World!")
